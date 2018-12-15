49a8103c3cf69a917728ad1820145bb7fdf4fbfc
coder-zzk <2238109333@qq.com>
2018-12-15 22:15:52 +0800

    add 16 02_使用mysql.py

---
+++ 16_访问数据库/02_使用mysql.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+Created on 2018年12月15日
+使用mysql
+@author: zzk
+'''
+# 导入MySQL驱动:
+import mysql.connector
+conn = mysql.connector.connect(user='root',password='',database='python')
+# 创建游标
+cursor=conn.cursor()
+def create_table_users():
+    global cursor
+    #执行一条SQL语句，创建一个users表
+    cursor.execute('''create table users(
+        id varchar(20) primary key,
+        name varchar(20)
+    )''')
+    # 继续执行一条SQL语句，插入一条记录:
+    cursor.execute('insert into users values(\'1001\',\'zzk\')')
+    print('插入行数',cursor.rowcount)
+    #关闭cursor
+    cursor.close()
+    #提交事务
+    conn.commit()
+    # 关闭连接
+    conn.close()
+#查询根据sql语句
+def select_table_users(select_statement):
+    global cursor
+    cursor.execute(select_statement)
+    values = cursor.fetchall()
+    return values
+print(select_table_users('select * from users'))
